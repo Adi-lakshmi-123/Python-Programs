0409f62d963571f5734f579fce8783ae904f76f7
Adi-lakshmi-123 <adilakshmikodimela@gmail.com>
2025-05-01 18:31:41 +0530

    Update Factorial.py

--- Recursion_programs/Factorial.py
+++ Recursion_programs/Factorial.py
@@ -3,13 +3,15 @@ Factorial is used to find the multiplies the given number by integers less or eq
 Ex: Factorial of 5: 5*4*3*2*1=120.
 '''
 #Program:
+import sys
 def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
         return n * factorial(n-1)
-n = 10
-if n < 0:
-    print("It is a negative number")
-else:
-    print(factorial(n))
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Pls provide integer on command line")
+    else:
+        n=int(sys.argv[1])
+        print(factorial(n))
