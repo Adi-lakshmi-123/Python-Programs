7c053873fc4218f619a42559fbb6ca4a254dcffc
Adilakshmi Kodimela <adilakshmikodimela@gmail.com>
2025-05-23 20:27:04 +0530

    Update Write_File.py

--- File_Operations/Write_File.py
+++ File_Operations/Write_File.py
@@ -1,3 +1,16 @@
-f1=open("File_Operations/NewFile.txt","a")
-f1.write("Hello world!This is writing text into a file.")
-f1.close()
+'''This is a file to add data into the text'''
+
+
+def write_file():
+    filename = "File_Operations/NewFile.txt"  # Extracting the path of file.
+    data = "This is a sample text written to the file."
+
+    try:
+        with open(filename, 'a') as file:  # appending data to the existing file.
+            file.write(data)
+            print(f"Successfully wrote to '{filename}'.")
+    except IOError as e:
+        print(f"An I/O error occurred: {e}")
+
+if _name_ == "_main_":
+    write_file()
